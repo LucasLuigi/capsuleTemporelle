570a053a958383646a41f1768f4d4d2a8d12373b
LucasLuigi <lukilucas@hotmail.fr>
2021-03-17 01:51:26 +0100

    Forgotten file in previous commit

--- capsuleTemporelle.py
+++ capsuleTemporelle.py
@@ -4,12 +4,21 @@ import sys
 import argparse
 import time
 
+from pathlib import Path
+from shutil import copyfile
+
 from dailyMode import dailyMode
 
 
-if __name__ == '__main__':
-    print('- capsuleTemporelle- \n\n')
+def firstExecution():
+    copyfile('templates\saved_context.template', 'saved_context.ini')
 
+
+if __name__ == '__main__':
+    print('- capsuleTemporelle- \n')
+
+    # Argument Parser         #
+    ###########################
     parser = argparse.ArgumentParser(
         description='Gestionnaire de capsules temporelles')
     parser.add_argument('-d', action='store_true', dest='flagDaily',
@@ -18,14 +27,30 @@ if __name__ == '__main__':
                         help='Add - Ajoute un fichier a la capsule temporelle. Doit etre suivi du chemin du fichier')
     args = parser.parse_args()
 
+    # First execution         #
+    ###########################
+    iniFile = Path('./saved_context.ini')
+    if not iniFile.is_file():
+        # Create .ini file in case of first execution
+        firstExecution()
+
+    # Daily mode              #
+    ###########################
     if(args.flagDaily == True):
-        # Daily execution
         # Reading saved context file from previous execution
-        dailyModeInst = dailyMode()
+        dailyMode = dailyMode()
+
+        # Run missed daily modes, including today's one
+        dailyMode.run()
+
+        # Save changed values offline to end the daily mode execution
+        # COMMENTED FOR TEST PURPOSE ONLY
+        # dailyMode.saveAndEnd()
     else:
+        # Add mode                #
+        ###########################
         if args.fileToAdd != None:
             # Add mode
             print('Ajout de '+args.fileToAdd +
                   ' a la liste des capsules temporelles.\n')
-
     time.sleep(3)
